41a41d5bfe1beb2918110aa8d91dde0ddadcc3ea
d-kawamoto <62988692+d-kawamoto@users.noreply.github.com>
2021-09-13 14:44:14 +0900

    first commit

---
+++ src/main/java/com/testapp/BranchTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchTestAppApplication.class, args);
+	}
+
+}
